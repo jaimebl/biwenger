ca7e47b95c26bbd0d014725897858d62e8bff4ee
Jaime Bergas Laborda <jaime84@gmail.com>
2020-02-18 15:00:56 +0000

    move pretty_time_ago to its own module

--- biwenger.py
+++ biwenger.py
@@ -1,5 +1,6 @@
 from biwengerClient import BiwengerClient
 from config_parser import properties
+from time_ago import pretty_time_ago
 
 from tabulate import tabulate
 from argparse import ArgumentParser
@@ -116,51 +117,8 @@ def millis_to_formatted_date(millis):
     return int(millis_to_date(millis).strftime("%y%m%d"))
 
 
-def pretty_date(time=False):
-    """
-    Get a datetime object or a int() Epoch timestamp and return a
-    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
-    'just now', etc
-    """
-    now = datetime.now()
-    if type(time) is int:
-        diff = now - datetime.fromtimestamp(time)
-    elif isinstance(time, datetime):
-        diff = now - time
-    elif not time:
-        diff = now - now
-    second_diff = diff.seconds
-    day_diff = diff.days
-
-    if day_diff < 0:
-        return ''
-
-    if day_diff == 0:
-        if second_diff < 10:
-            return "just now"
-        if second_diff < 60:
-            return str(second_diff) + " seconds ago"
-        if second_diff < 120:
-            return "a minute ago"
-        if second_diff < 3600:
-            return str(round(second_diff / 60)) + " minutes ago"
-        if second_diff < 7200:
-            return "an hour ago"
-        if second_diff < 86400:
-            return str(round(second_diff / 3600)) + " hours ago"
-    if day_diff == 1:
-        return "Yesterday"
-    if day_diff < 7:
-        return str(day_diff) + " days ago"
-    if day_diff < 31:
-        return str(round(day_diff / 7)) + " weeks ago"
-    if day_diff < 365:
-        return str(round(day_diff / 30)) + " months ago"
-    return str(round(day_diff / 365)) + " years ago"
-
-
-def full_pretty_date(time):
-    return f'{pretty_date(time)} ({millis_to_date(time)})'
+def full_pretty_time_ago(time):
+    return f'{pretty_time_ago(time)} ({millis_to_date(time)})'
 
 
 # def team_value_by_date(team, date):
@@ -257,7 +215,7 @@ def players_ranking():
                    money(player["cash"]),
                    money(player["maxBid"]),
                    money(player["dailyIncrement"]),
-                   full_pretty_date(player["lastAccess"])]
+                   full_pretty_time_ago(player["lastAccess"])]
                   for player in players]
 
     print(tabulate(print_data,
--- config_parser.py
+++ config_parser.py
@@ -1,11 +1,11 @@
-separator = "="
+_separator = "="
 
 def _load_config():
     properties = {}
     with open('config.properties', mode='rt', encoding='utf-8') as f:
         for line in f:
-            if separator in line:
-                name, value = line.split(separator, 1)
+            if _separator in line:
+                name, value = line.split(_separator, 1)
                 properties[name.strip()] = value.strip()
 
     return properties
